205a70da99416b6b9bad548a722f5175a1d79d29
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-05-30 13:55:12 +0000

    1.3 -> 1.3.0, Staðarnet as preset, tweaks in voice selection controller

--- Embla/Controllers/VoiceSelectionViewController.m
+++ Embla/Controllers/VoiceSelectionViewController.m
@@ -23,12 +23,11 @@
 #import "DataURI.h"
 #import "Common.h"
 
-#define FALLBACK_VOICES     @[@"Dora", @"Karl"]
+#define FALLBACK_VOICES     @[@"Dóra", @"Karl"]
 
 @interface VoiceSelectionViewController ()
 
 @property (nonatomic, strong) UIActivityIndicatorView *progressView;;
-@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
 @property (nonatomic, strong) NSArray *voices;
 
 @end
@@ -39,8 +38,7 @@
     [super viewDidLoad];
     
     self.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
-    
-    self.voices = FALLBACK_VOICES;
+    self.voices = @[];
     
 #ifdef DEBUG
     // Load list of voices from remote server when in debug mode
@@ -76,6 +74,8 @@
     };
     
     [[QueryService sharedInstance] requestVoicesWithCompletionHandler:completionHandler];
+#else
+    self.voices = FALLBACK_VOICES;
 #endif
 
     [self.tableView reloadData];
@@ -94,116 +94,8 @@
     [DEFAULTS synchronize];
     DLog(@"Set VoiceID to \"%@\"", voiceName);
     [self.tableView reloadData];
-//    [self.navigationController popViewControllerAnimated:YES];
-    
-    id synthesisCompletionHandler = ^(NSURLResponse *response, id responseObject, NSError *error) {
-        NSDictionary *respDict = (NSDictionary *)responseObject;
-        NSString *audioURLStr = [respDict objectForKey:@"audio_url"];
-        if ([[respDict objectForKey:@"err"] boolValue] || !audioURLStr) {
-            return;
-        }
-        [self playRemoteURL:audioURLStr];
-    };
-    
-    // Speech synthesise text via Greynir API and play
-    NSString *introductionMsg = [NSString stringWithFormat:@"Ég heiti %@ og ég hljóma svona", voiceName];
-    [[QueryService sharedInstance] requestSpeechSynthesis:introductionMsg
-                                        completionHandler:synthesisCompletionHandler];
-
+    [self.navigationController popViewControllerAnimated:YES];
 }
-
-- (void)playAudio:(id)filenameOrData {
-    [self playAudio:filenameOrData rate:1.0];
-}
-
-// Utility function that creates an AVAudioPlayer to play either a local file or audio data
-- (void)playAudio:(id)filenameOrData rate:(float)rate {
-    NSAssert([filenameOrData isKindOfClass:[NSString class]] || [filenameOrData isKindOfClass:[NSData class]],
-             @"playAudio argument neither filename string nor data.");
-    
-    NSError *err;
-    AVAudioPlayer *player;
-    
-    if ([filenameOrData isKindOfClass:[NSString class]]) {
-        // Local filename specified, init player with local file URL
-        NSString *filename = (NSString *)filenameOrData;
-        NSURL *url = [[NSBundle mainBundle] URLForResource:filename withExtension:@"wav"];
-        if (url) {
-            DLog(@"Playing audio file '%@'", filename);
-            player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&err];
-        } else {
-            NSString *errStr = [NSString stringWithFormat:@"Unable to find audio file '%@' in bundle", filename];
-            err = [NSError errorWithDomain:@"Embla" code:0 userInfo:@{ NSLocalizedDescriptionKey: errStr }];
-        }
-    }
-    else {
-        // Init player with audio data
-        NSData *data = (NSData *)filenameOrData;
-        player = [[AVAudioPlayer alloc] initWithData:data error:&err];
-        DLog(@"Playing audio data (%.2f seconds, size %d bytes)", player.duration, (int)[data length]);
-    }
-    
-    if (err) {
-        DLog(@"%@", [err localizedDescription]);
-        return;
-    }
-    
-    // Configure player and set it off
-    self.audioPlayer = player;
-    [self.audioPlayer setVolume:1.0];
-    if (rate != 1.0) {
-        self.audioPlayer.enableRate = YES;
-        [self.audioPlayer setRate:rate];
-    }
-//    [player setMeteringEnabled:YES];
-    [player play];
-}
-
-
-// Download remote MP3 file and play it when download is complete
-- (void)playRemoteURL:(NSString *)urlString {
-    
-    // Special handling of Data URIs
-    if ([DataURI isDataURI:urlString]) {
-        DataURI *uri = [[DataURI alloc] initWithString:urlString];
-        NSData *data = [uri data];
-        if (uri == nil || data == nil) {
-            NSString *msg = [NSString stringWithFormat:@"Failed to decode Data URI %@", urlString];
-            NSError *error = [NSError errorWithDomain:@"Embla" code:0 userInfo:@{ NSLocalizedDescriptionKey:msg }];
-            DLog(@"Error fetching audio: %@", [error localizedDescription]);
-            return;
-        }
-        [self playAudio:data];
-        return;
-    }
-    
-    NSURL *url = [NSURL URLWithString:urlString];
-    DLog(@"Downloading audio URL: %@", [url description]);
-
-    NSURLSessionDataTask *downloadTask = \
-    [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        DLog(@"Response was: %@", [response description]);
-                
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        NSDictionary *headerFields = [httpResponse allHeaderFields];
-        
-        // Make sure content-type is audio/mpeg
-        NSString *contentType = [headerFields objectForKey:@"Content-Type"];
-        if (!error && (!contentType || ![contentType isEqualToString:@"audio/mpeg"])) {
-            NSString *msg = [NSString stringWithFormat:@"Wrong content type from speech audio server: %@", contentType];
-            error = [NSError errorWithDomain:@"Embla" code:0 userInfo:@{ NSLocalizedDescriptionKey:msg }];
-        }
-        
-        if (error) {
-            DLog(@"Error downloading audio: %@", [error localizedDescription]);
-            return;
-        }
-        DLog(@"Commencing audio answer playback");
-        [self playAudio:data];
-    }];
-    [downloadTask resume];
-}
-
 
 #pragma mark - UITableViewDataSource
 
